177b2e3fce989ea8638bfc1beb3c1d5a8e3324f9
Casper van Gameren <124160066+CaspervGameren@users.noreply.github.com>
2025-11-04 14:38:49 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const functionality = [
+    {
+        title: "Stap 1",
+        content: "De machine wordt bestuurd met een controller die de lopende band zal activeren.",
+    },
+    {
+        title: "Stap 2",
+        content: "Het lego stukje wordt over de band vervoerd naar onder de kleur lees machine.",
+    }, 
+    {
+        title: "Stap 3",
+        content: "De kleur lees machine, scant het lego blokje door licht terug te kaatsen naar de sensor",
+    },
+    {
+        title: "Stap 4",
+        content: "De sensor leest de kleur en stuurt deze informatie naar de servos.",
+    },
+     {
+        title: "Stap 5",
+        content: "De servos ontvangen de kleurinformatie en passen hun positie aan. Om zo het lego blokje in de juiste bak te laten vallen.",
+    },
+     {
+        title: "Stap 6",
+        content: "Het proces wordt herhaald voor elk lego blokje dat op de lopende band komt.",
+    },
+]
+
+export default functionality;
